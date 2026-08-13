5ff1023e8216a56b8e393f247671222bce776126
hadday50 <91617257+hadday50@users.noreply.github.com>
2026-08-13 17:13:50 +0200

    Change Hello World message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hej Kollegor på DigInn! Min forsta OpenShift-applikation!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
